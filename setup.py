92f8b3f4026b4310b4abba9590a971ec751cce24
carnarez <11967125+carnarez@users.noreply.github.com>
2021-10-30 20:06:27 +0200

    Fixed mypy demanding demands

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 """Make `markdown-astdocs` installable (via `pip install git+https://...`)."""
 
-import setuptools
+import setuptools  # type: ignore
 
 setuptools.setup(
     author="carnarez",
     description="Add support for astdocs output %%% markers to Python-Markdown.",
-    install_requires = ["markdown"],
+    install_requires=["markdown"],
     name="markdown-astdocs",
+    package_data={"": ["*.pyi"]},
     py_modules=["markdown_astdocs"],
     url="https://github.com/carnarez/markdown-astdocs",
     version="0.0.1",
